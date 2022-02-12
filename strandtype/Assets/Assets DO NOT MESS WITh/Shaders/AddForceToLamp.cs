e0b2059bb2cb553baf2717146eb279f4327d900e
Maxwell Home <maxwellrichter1@gmail.com>
2022-02-12 11:44:40 -0800

    lighting

---
+++ strandtype/Assets/Assets DO NOT MESS WITh/Shaders/AddForceToLamp.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddForceToLamp : MonoBehaviour
+{
+
+    private bool waiting = false;
+    public Rigidbody theRB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!waiting)
+        {
+            StartCoroutine("Swing");
+        }
+    }
+
+
+
+
+    IEnumerator Swing()
+    {
+        waiting = true;
+        theRB.AddForce(1f, 0f, 0f);
+        Debug.Log("adds force");
+        yield return new WaitForSeconds(.01f);
+        waiting = false;
+
+    }
+
+}
