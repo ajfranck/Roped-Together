6eadbf75514d3c4762dbcab3b078ec9d2e9c5e88
Miles Anderson <manderson0045@student.pps.net>
2022-03-16 12:34:11 -0700

    More ladder

--- strandtype/Assets/LaderScript.cs
+++ strandtype/Assets/LaderScript.cs
@@ -6,18 +6,38 @@ public class LaderScript : MonoBehaviour
 {
     public GameObject LadderSegment;
     public GameObject Player1;
+    public int degrees;
     void Start()
     {
+    }
 
-
+    public Vector3 PositionClass()
+    {
+        Vector3 SpawnPosition = this.transform.position;
+        SpawnPosition.y = SpawnPosition.y + 3;
+        if(this.transform.rotation.y > 0)
+        {
+        SpawnPosition.x = SpawnPosition.x + 2;
+        }
+        if(this.transform.rotation.y < 0)
+        {
+            SpawnPosition.x = SpawnPosition.x - 2;
+        }
+        
+        return SpawnPosition;
 
     }
+
+
+
     void Update()
     {
         if(Input.GetKeyDown("space"))
         {
-            GameObject Ladder = Instantiate(LadderSegment,Player1.transform.position, Player1.transform.rotation);
-            Debug.Log("Ladder rotation: " + Ladder.transform.rotation.x);
+            Vector3 SpawnPosition = PositionClass();
+            GameObject Ladder = Instantiate(LadderSegment, SpawnPosition, this.transform.rotation);
+            //Ladder.transform.rotation = Quaternion.Euler(Vector3.forward + degrees);
+            
         }
 
 
@@ -26,11 +46,7 @@ public class LaderScript : MonoBehaviour
     }
 
 
-    void InstantateLadders()
-    {
-
-    }
-
+  
   
 
 
