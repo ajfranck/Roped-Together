92a9009e48f1499bc7015e8d3966091e61d6c5be
adsdf <maxwellrichter1@gmail.com>
2022-03-30 12:33:57 -0700

    started walking up wall

--- strandtype/Assets/Player1/MovementController.cs
+++ strandtype/Assets/Player1/MovementController.cs
@@ -15,7 +15,11 @@ public class MovementController : MonoBehaviour
 
     // variables to store player input 
     Vector2 currentMovementInput;
+
     Vector3 currentMovement;
+    
+    Vector3
+
     bool movementPressed;
     float rotationFactor = 10f;
 
@@ -26,6 +30,7 @@ public class MovementController : MonoBehaviour
 
     public WarmthBar warmthbar;
 
+    public bool ClimbRope = false;
 
 
 
@@ -36,9 +41,16 @@ public class MovementController : MonoBehaviour
         characterController = GetComponent<CharacterController>();
         animator = GetComponent<Animator>();
 
-        playerInput.CharacterController.Move.started += onMovementInput;
-        playerInput.CharacterController.Move.canceled += onMovementInput;
-        playerInput.CharacterController.Move.performed += onMovementInput;
+        //playerInput.CharacterController.Move.started += onMovementInput;
+        //playerInput.CharacterController.Move.canceled += onMovementInput;
+        //playerInput.CharacterController.Move.performed += onMovementInput;
+
+
+
+        //rope movement
+        playerInput.CharacterController.Move.started += RopeMove;
+        playerInput.CharacterController.Move.canceled += RopeMove;
+        playerInput.CharacterController.Move.performed += RopeMove;
 
         currentMovement.x = currentMovementInput.x;
         currentMovement.z = currentMovementInput.y;
@@ -50,12 +62,30 @@ public class MovementController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // if(!AtFire){
-        characterController.Move(currentMovement * Time.deltaTime * 10f);
-        Rotation();
 
-        Gravity();
-        //}
+
+
+        if (ClimbRope)
+        {
+
+
+            RopeRotation();
+            characterController.Move(rope * Time.deltaTime);
+
+        }
+        else
+        {
+            characterController.Move(currentMovement * Time.deltaTime * 10f);
+            Rotation();
+            Gravity();
+        }
+
+
+
+     
+
+       // Gravity();
+        
 
         if (warmthbar.isInteracting)
         {
@@ -68,9 +98,9 @@ public class MovementController : MonoBehaviour
     }
     void FixedUpdate()
     {
-        //if(!AtFire){        
+            
         handleAnimation();
-        //}
+       
     }
 
     void onMovementInput(InputAction.CallbackContext context)
@@ -122,7 +152,8 @@ public class MovementController : MonoBehaviour
 
         //telling it to look at the direction we are moving
         positionToLook.x = currentMovement.x;
-        positionToLook.y = currentMovement.y = 0f;
+        // positionToLook.y = currentMovement.y = 0f;
+        positionToLook.y = currentMovement.y;
         positionToLook.z = currentMovement.z;
 
 
@@ -149,5 +180,52 @@ public class MovementController : MonoBehaviour
     }
 
 
+
+    void RopeMove(InputAction.CallbackContext context)
+    {
+        currentMovementInput = context.ReadValue<Vector2>();
+        currentMovement.x = currentMovementInput.x;
+        currentMovement.y = currentMovementInput.y;
+        movementPressed = currentMovement.x != 0 || currentMovementInput.y != 0;
+    }
    
+
+
+    void RopeRotation()
+    {
+        Vector3 positionToLook;
+
+        //telling it to look at the direction we are moving
+        positionToLook.x = currentMovement.x;
+        // positionToLook.y = currentMovement.y = 0f;
+
+        positionToLook.y = currentMovement.y;
+
+        if(positionToLook.y == -1)
+        {
+            positionToLook.y = 1;
+        }
+
+        positionToLook.z = currentMovement.z;
+        Debug.Log("PositionLook" + positionToLook.y);
+
+
+        Quaternion currentRotation = transform.rotation; // character's current rotation
+        if (movementPressed)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(positionToLook);
+            transform.rotation = Quaternion.Slerp(currentRotation, targetRotation, rotationFactor * Time.deltaTime);
+        }
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Climbwall")) 
+        {
+            ClimbRope = true;
+        }
+    }
+
+
 }
