516618c06998819e88f8dd06349df49798eb6542
Andy <1amajfranck@gmail.com>
2022-04-29 12:20:34 -0700

    finished item pickup script

--- strandtype/Assets/Healing.cs
+++ strandtype/Assets/Healing.cs
@@ -9,24 +9,36 @@ public class Healing : MonoBehaviour
     public HotBar hotbar;
     public WarmthBar warmthbar;
 
+    public Animator animator;
+
     float warmthStart;
 
     void Update()
     {
         if (Input.GetKey("e") && HotBars.HotBarListP1[HotBars.HotBarPositionP1] != null && HotBars.HotBarListP1[HotBars.HotBarPositionP1].name == "BowlItem" && !mining.isMining && !warmthbar.isInteracting)
         {
-            HotBars.HotBarListP1[HotBars.HotBarPositionP1] = null;
-            hotbar.HotBarSpritesP1[HotBars.HotBarPositionP1].GetComponent<Image>().sprite = hotbar.BackgroundImage.GetComponent<Image>().sprite;
-
-            warmthStart = warmthbar.P1currentWarmth;
-            if(warmthStart + 50 > warmthbar.P1MaxWarmth) warmthStart = warmthbar.P1MaxWarmth - 1;
-            else warmthStart += 50;
-            while(warmthbar.P1currentWarmth < warmthStart)
-            {
-                warmthbar.gainWarmth(0.5f);
-            }
-            
+            StartCoroutine(UseIt());
         }
     }
+
+    IEnumerator UseIt()
+    {
+        hotbar.isGrabbing = true;
+        animator.SetTrigger("Grab");
+        yield return new WaitForSeconds(1.5f);
+        
+        HotBars.HotBarListP1[HotBars.HotBarPositionP1] = null;
+        hotbar.HotBarSpritesP1[HotBars.HotBarPositionP1].GetComponent<Image>().sprite = hotbar.BackgroundImage.GetComponent<Image>().sprite;
+        
+        warmthStart = warmthbar.P1currentWarmth;
+        if(warmthStart + 50 > warmthbar.P1MaxWarmth) warmthStart = warmthbar.P1MaxWarmth - 1;
+        else warmthStart += 50;
+        while(warmthbar.P1currentWarmth < warmthStart)
+        {
+            warmthbar.gainWarmth(0.5f);
+        }
+        hotbar.isGrabbing = false;
+    }
+
 
 }
